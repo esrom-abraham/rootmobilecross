667fe26286177b5c56717c4fff886e8dad4c3468
Esrom Abraham <99913300+esrom-abraham@users.noreply.github.com>
2022-03-08 11:21:38 -0800

    Updated

--- Newfolder/demo.js
+++ Newfolder/demo.js
@@ -1,2 +1,7 @@
-console.log("Hello there");
 document.getElementById("print").innerHTML = "Oh....hello there";
+
+var teacher= "Hello";
+var student= "world";
+
+var school= teacher+student
+document.getElementById("print").innerHTML = "Hi"+school;
