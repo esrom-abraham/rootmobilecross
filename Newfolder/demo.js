e73e7e6d2f2d4d6fa4674d81633b3bfe8262ea32
Esrom Abraham <99913300+esrom-abraham@users.noreply.github.com>
2022-03-17 10:15:23 -0700

    Delete Newfolder directory

--- Newfolder/demo.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("print").innerHTML = "Oh....hello there";
-
-var teacher= "How are ";
-var student= "you?";
-
-var school= teacher+student
-document.getElementById("print").innerHTML = "Hi, "+school;
