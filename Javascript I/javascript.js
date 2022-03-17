45ab993fe66bc9b34cd814f0dd0973a814fa5b96
Esrom Abraham <99913300+esrom-abraham@users.noreply.github.com>
2022-03-17 10:28:50 -0700

    Update javascript.js

--- Javascript I/javascript.js
+++ Javascript I/javascript.js
@@ -13,15 +13,20 @@ newvar = newnum + 5;
 document.getElementById("coolerheader").innerHTML =
   newnum + " and " + newvar + " are my two favorite numbers";
 
-var first = prompt("Say something.................................'hello' ");
+var first = prompt("Hello user. I'm thinking of a number from 1-100. Can you guess what I'm thinking of?");
 
 while (newnum === 7) {
-  if (first === "hello") {
+  if (first === "69") {
     alert("Wow you're pretty smart. For that, here's my social security number...1234 56 7891");
     break;
   } 
   
   else {
-    prompt("Sorry try again");
+    let x = prompt("Sorry try again");
+    
+    if (x === "69") {
+      alert("Finally you got it");
+      break;
+    }
   }
 }
