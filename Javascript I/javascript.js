ec9a40d4af614034ff331947b6125667b7bc34f5
Esrom Abraham <99913300+esrom-abraham@users.noreply.github.com>
2022-03-17 10:17:51 -0700

    Add files via upload

---
+++ Javascript I/javascript.js
@@ -0,0 +1,26 @@
+console.log("Hmmmm.....What are you doing here");
+
+document.getElementById("header").innerHTML = "HIIIII!!!!!!";
+
+var newnum;
+
+newnum = 7;
+
+var newvar;
+
+newvar = newnum + 5;
+
+document.getElementById("coolerheader").innerHTML =
+  newnum + " and " + newvar + " are my two favorite numbers";
+
+var first = prompt("Say something.................................'hello' ");
+
+while (newnum === 7) {
+  if (first === "hello") {
+    alert(
+      "Wow you're pretty smart. For that, here's my social security number...1234 56 7891"
+    );
+  } else {
+    prompt("Sorry try again");
+  }
+}
