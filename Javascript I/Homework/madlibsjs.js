78297c423433766967de42aa4e2ecbf7dc47ba11
Esrom Abraham <99913300+esrom-abraham@users.noreply.github.com>
2022-03-17 11:12:33 -0700

    Create madlibsjs.js

---
+++ Javascript I/Homework/madlibsjs.js
@@ -0,0 +1,5 @@
+document.getElementById("thename").innerHTML = prompt("What is your name?")
+
+document.getElementById("food").innerHTML = prompt("What food do you like to eat?")
+
+document.getElementById("flying").innerHTML = prompt("If you could fly anywhere, where would you fly to?")
