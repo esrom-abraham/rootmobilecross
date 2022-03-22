7190593bdda41060120d5bce508203b1bb7affd8
Esrom Abraham <99913300+esrom-abraham@users.noreply.github.com>
2022-03-22 10:29:08 -0700

    Update madlibsjs.js

--- Javascript I/Homework/madlibsjs.js
+++ Javascript I/Homework/madlibsjs.js
@@ -2,4 +2,4 @@ document.getElementById("thename").innerHTML = prompt("What is your name?")
 
 document.getElementById("food").innerHTML = prompt("What food do you like to eat?")
 
-document.getElementById("flying").innerHTML = prompt("If you could fly anywhere, where would you fly to?")
+document.getElementById("flying").innerHTML = prompt("What planet would you fly to? (Think outside real planets)")
