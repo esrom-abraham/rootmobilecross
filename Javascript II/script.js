5639cff8fe46cb6de460ba0469f7d80ba55eb683
Esrom Abraham <99913300+esrom-abraham@users.noreply.github.com>
2022-04-14 21:55:31 -0700

    Update script.js

--- Javascript II/script.js
+++ Javascript II/script.js
@@ -26,62 +26,68 @@ function draw() {
 
     var stardraw = canvas.getContext("2d");
     
+    var universalwidth = 27
+    var bordery = 500
+    
     startdraw.fillStyle = "rgba(255, 0, 0)";
-    startdraw.fillRect(0, 0, 370, 487);
+    startdraw.fillRect(0, 0, 370, bordery-13);
     
     //Big red rectangle
-    startdraw.lineWidth = 27;
+    startdraw.lineWidth = universalwidth;
     startdraw.beginPath(); // Start a new path
     startdraw.moveTo(381, 0); // Move the pen to ( x1, y1 )
     startdraw.lineTo(381, 960); // Draw a line to (x2, y2)
     startdraw.stroke(); // Render the path
     
     //Line right below the red rectangle
-    startdraw.lineWidth = 27;
+    startdraw.lineWidth = universalwidth;
     startdraw.beginPath(); 
-    startdraw.moveTo(0, 487); 
-    startdraw.lineTo(381, 487);
+    startdraw.moveTo(0, bordery-13); 
+    startdraw.lineTo(381, bordery-13);
     startdraw.stroke();
     
     //Line in the middle
-    startdraw.lineWidth = 27;
+    startdraw.lineWidth = universalwidth;
     startdraw.beginPath(); 
-    startdraw.moveTo(27, 487); 
-    startdraw.lineTo(920, 487);
+    startdraw.moveTo(27, bordery-13); 
+    startdraw.lineTo(920, bordery-13);
     startdraw.stroke();
     
     //Line next to the blue rectangle
-    startdraw.lineWidth = 27;
+    startdraw.lineWidth = universalwidth;
     startdraw.beginPath(); 
-    startdraw.moveTo(826, 487); 
+    startdraw.moveTo(826, bordery-13); 
     startdraw.lineTo(826, 900);
     startdraw.stroke();
     
     //Blue rectangle
     startdraw.fillStyle = "rgba(0, 0, 255)";
-    startdraw.fillRect(839, 500, 81, 140);
+    startdraw.fillRect(839, bordery, 81, 140);
     
     //Thick line
-    startdraw.lineWidth = 37;
+    startdraw.lineWidth = universalwidth + 10;
     startdraw.beginPath(); 
     startdraw.moveTo(839, 622); 
     startdraw.lineTo(920, 622);
     startdraw.stroke();
   
+    var rectdimension1 = 600
+    var rectdimension2 = 210
+    
     //Line on top of yellow rectangle  
-    startdraw.lineWidth = 27;
+    startdraw.lineWidth = universalwidth;
     startdraw.beginPath(); 
-    startdraw.moveTo(390, 700); 
+    startdraw.moveTo(rectdimension1-rectdimension2, 700); 
     startdraw.lineTo(830, 700);
     startdraw.stroke();
     
     
     //Yellow rectangle
     startdraw.fillStyle = "rgba(255, 255, 0)";
-    startdraw.fillRect(390, 713, 230, 140);
+    startdraw.fillRect(rectdimension1-rectdimension2, 713, 230, 140);
     
     //Line between yellow rectangle and black rectangle
-    startdraw.lineWidth = 27;
+    startdraw.lineWidth = universalwidth;
     startdraw.beginPath(); 
     startdraw.moveTo(609, 700); 
     startdraw.lineTo(609, 900);
@@ -91,7 +97,7 @@ function draw() {
     startdraw.fillRect(623, 713, 189, 120);
     
     //The line to the right
-    startdraw.lineWidth = 27;
+    startdraw.lineWidth = universalwidth;
     startdraw.beginPath(); 
     startdraw.moveTo(920, 0); 
     startdraw.lineTo(920, 900);
