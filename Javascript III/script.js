126c3fe16fe552b0404561486886a97f776c49c2
Esrom Abraham <99913300+esrom-abraham@users.noreply.github.com>
2022-04-29 21:26:52 -0700

    Create script.js

---
+++ Javascript III/script.js
@@ -0,0 +1,34 @@
+let canvas;
+let startdraw;
+
+let increment = 0;
+
+let speed = 5;
+
+window.onload = function () {
+  canvas = document.getElementById("animation");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  startdraw = canvas.getContext("2d");
+
+  setInterval(draw, 20);
+};
+
+function draw() {
+  increment = increment + speed;
+
+  startdraw.fillStyle = "green";
+  startdraw.fillRect(0, 0, canvas.width, canvas.height);
+
+  startdraw.fillStyle = "rgba(255, 0, 0)";
+  startdraw.fillRect(0, increment, 100, 100);
+
+  startdraw.fillStyle = "rgba(173, 224, 98)";
+  startdraw.fillRect(increment, 0, 110, 110);
+
+  startdraw.fillStyle = "rgba(221, 34, 48)";
+  startdraw.fillRect(10, 10, 10 + increment, 10 + increment);
+
+  startdraw.fillStyle = "rgba(0, 0, 255)";
+  startdraw.fillRect(0, 0, canvas.width - increment, canvas.height - increment);
+}
